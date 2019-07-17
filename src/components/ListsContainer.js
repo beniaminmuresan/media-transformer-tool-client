64db2abaf8ced2b39f8c26266ed4eb27d3957049
muresanbeniamin <ben.muresan@luminos.software>
2019-07-17 08:51:31 +0300

    changed request

--- src/components/ListsContainer.js
+++ src/components/ListsContainer.js
@@ -8,7 +8,7 @@ class ListsContainer extends Component {
     }
   }
   componentDidMount() {
-    axios.get('api/v1/lists.json')
+    axios.get('api/v1/lists')
     .then(response => {
       console.log(response)
       this.setState({
