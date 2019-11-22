996bd8672c53ebd475db2171d127da97f59cfd3d
Muresan Beniamin <muresan.beniamin@hotmail.com>
2019-11-22 17:41:24 +0200

    changed signup form

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { fileURLToPath } from 'url';
+import { reduxForm, Field } from 'redux-form';
 
 class SignUp extends Component {
+
+  onSubmit = (formProps) => {
+    console.log(formProps);
+  };
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
+          <Field 
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field 
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
+        <button>Sign Up!</button>
       </form>
     );
   }
 };
 
-export default SignUp;
+export default reduxForm({ form: 'signup' })(SignUp);
