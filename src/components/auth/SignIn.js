87066ee4161f74182d1c5d1acb8be0ee71677a8a
Muresan Beniamin <ben.muresan@luminos.software>
2019-11-26 17:28:25 +0200

    added material ui to sign in component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -4,6 +4,82 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © DoItRight '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const renderTextField = ({
+  label,
+  placeholder,
+  required,
+  variant,
+  margin,
+  fullWidth,
+  id,
+  name,
+  autoComplete,
+  autoFocus,
+  input,
+  ...custom
+}) => (
+  <TextField
+    label={label}
+    placeholder={label}
+    required={required}
+    variant={variant}
+    margin={margin}
+    fullWidth={fullWidth}
+    id={id}
+    name={name}
+    autoComplete={autoComplete}
+    autoFocus={autoFocus}
+    {...input}
+    {...custom}
+  />
+)
+
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing(20),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    width: 70,
+    height: 70,
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
+
 class SignIn extends Component {
 
   onSubmit = formProps => {
@@ -14,30 +90,68 @@ class SignIn extends Component {
 
   render() {
     const { handleSubmit } = this.props;
+    const { classes } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <fieldset>
-          <label>Email</label>
-          <Field 
-            name="email"
-            type="text"
-            component="input"
-            autoComplete="none"
-          />
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-          <Field 
-            name="password"
-            type="password"
-            component="input"
-            autoComplete="none"
-          />
-        </fieldset>
-        <div> {this.props.errorMessage} </div>
-        <button>Sign In!</button>
-      </form>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <img height="60px" width="60px" alt="DoItRight" src="/images/doitright.png" />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <form className={classes.form} onSubmit={handleSubmit(this.onSubmit)}>
+              <Field 
+                name="email"
+                type="text"
+                label="Email"
+                component={renderTextField}
+                autoComplete="none"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="email"
+                autoFocus
+              />
+              <Field 
+                name="password"
+                type="password"
+                label="Password"
+                component={renderTextField}
+                autoComplete="none"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="password"
+              />
+
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+            <div> {this.props.errorMessage} </div>
+            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>Sign In</Button>
+            <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     );
   }
 };
@@ -48,5 +162,6 @@ function mapStateToProps(state) {
 
 export default compose(
   connect(mapStateToProps, actions),
-  reduxForm({ form: 'signin' })
+  reduxForm({ form: 'signin' }),
+  withStyles(styles)
 )(SignIn);
