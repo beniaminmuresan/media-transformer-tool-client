c3b86163a74e02b4456522ab08ccfac4affb823b
Darius Otelea <otelea.darius@gmail.com>
2020-01-14 01:12:18 +0200

    Title to history page

--- src/components/history-list/history-list.component.js
+++ src/components/history-list/history-list.component.js
@@ -1,10 +1,14 @@
 import React from "react";
 import HistoryEntry from "../history-entry/history-entry.component";
+import Typography from "@material-ui/core/Typography";
 
 import "./history-list.style.scss";
 
 const HistoryList = () => (
   <div className="history-list">
+    <Typography style={{ marginLeft: "16%" }} variant="h4">
+      History
+    </Typography>
     <HistoryEntry />
     <HistoryEntry />
     <HistoryEntry />
